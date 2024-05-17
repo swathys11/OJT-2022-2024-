e44c3a4dca910c993ed1d64e3469ebd606fef548
swathys11 <144905167+swathys11@users.noreply.github.com>
2024-05-17 12:50:14 +0530

    Add files via upload

---
+++ DAY2/dict.py
@@ -0,0 +1,3 @@
+my_dict={"A":"Apple","B":"Banana","C":"Cherry"}
+for key,value in my_dict.items():
+    print("key",key,"value",value)
---
+++ DAY2/list.py
@@ -0,0 +1,3 @@
+my_list=[1,4,8,12,16]
+my_list.remove(8)
+print(my_list)
