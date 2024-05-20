2d60f76720dab8c72a8795c10eba4536bc7fcc65
Swathy S <144905167+swathys11@users.noreply.github.com>
2024-05-20 16:27:00 +0530

    Update factorial.py

--- DAY3/factorial.py
+++ DAY3/factorial.py
@@ -1,4 +1,5 @@
 #Write a Python program to calculate the factorial of a number using a lambda function and reduce function
+from functools import reduce
 def factorial(n):
     return reduce(lambda x, y: x * y, range(1, n + 1), 1)
 
