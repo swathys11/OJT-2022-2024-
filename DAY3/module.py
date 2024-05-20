d9ff8d850a31e8b3b7137b8231e7f21246b2e3ac
Swathy S <144905167+swathys11@users.noreply.github.com>
2024-05-20 16:31:10 +0530

    Update module.py

--- DAY3/module.py
+++ DAY3/module.py
@@ -2,6 +2,6 @@
 import math
 def calculate_square_root(number):
     return math.sqrt(number)
-number = 16
+number = 25
 result = calculate_square_root(number)
 print("The square root of ",number," is ",result)
