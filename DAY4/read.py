983dafcc8048329ede5353fb4d81248485009972
Swathy S <144905167+swathys11@users.noreply.github.com>
2024-05-21 16:44:32 +0530

    Delete DAY4 directory

--- DAY4/read.py
+++
@@ -1,8 +0,0 @@
-#Write a Python program to Read/Write to a File
-with open("file.txt", 'w') as file:
-    file.write("Hello, this is some content written to the file.")
-
-with open("file.txt", 'r') as file:
-    content = file.read()
-    print("Content read from the file:")
-    print(content)
