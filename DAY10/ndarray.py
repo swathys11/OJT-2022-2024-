b3c0a0407cd9e3ec6c47f67eaa1418e49c518e52
Swathy S <144905167+swathys11@users.noreply.github.com>
2024-05-30 16:23:46 +0530

    Add files via upload

---
+++ DAY10/ndarray.py
@@ -0,0 +1,9 @@
+import numpy as np
+# Creating a one-dimensional ndarray
+array_1d = np.array([1, 2, 3, 4, 5])
+print("1D array:", array_1d)
+
+# Creating a two-dimensional ndarray (matrix)
+array_2d = np.array([[1, 2, 3], [4, 5, 6]])
+print("2D array:\n", array_2d)
+
