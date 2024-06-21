665898eaf0d8c5e1a78bbf21abca39ad179b4d94
Swathy S <144905167+swathys11@users.noreply.github.com>
2024-06-21 14:06:13 +0530

    Add files via upload

---
+++ DAY25/TUPLE.PY
@@ -0,0 +1,22 @@
+# Joining/ Concatenation 
+# The + operator, concatenation operator is used to joining two tuples
+# eg 1
+tuplel = (1, 2, 3)
+tuple2 =(4,5,3,6)
+print (tuplel + tuple2)
+
+# eg 2
+tuplel = (1,2,3)
+tuple2 =(4,5,6)
+tuple3 =('a', 12.5, (7,8))
+print (tuplel + tuple2 + tuple3)
+
+
+# Repeating/ Replicate 
+# You can use * operator to replicate a tuple a specified number of times
+
+tuplel =(1,2,3)
+print(tuplel *4)
+
+# Slicing the Tuples 
+# Slicing refers to extracting a subpart of the mentioned sequence. Tuple- slices are the same as the List- slices
