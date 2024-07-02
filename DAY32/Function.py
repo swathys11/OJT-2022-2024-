bbaf5096464d49ebe7c9a1137b22ccde7fa979eb
Swathy S <144905167+swathys11@users.noreply.github.com>
2024-07-02 14:20:09 +0530

    Add files via upload

---
+++ DAY32/Function.py
@@ -0,0 +1,21 @@
+def add(a, b): 
+ print ("Adding : ", a + b, (a, b)) 
+ return a + b 
+# Function: Subtract
+def subtract(a, b): 
+ print ("Subtracting : ",a - b , (a, b)) 
+ return a - b 
+# Function: Multiply
+def multiply(a, b): 
+ print ("Multiplying : ", a * b,(a, b)) 
+ return a * b 
+# Function: Divide
+def divide(a, b): 
+    print ("Dividing : " ,a / b, (a, b)) 
+    return a / b 
+print ("Let's use the functions created by us") 
+# Calling Functions
+A = add(5, 5) 
+B = subtract(5, 6) 
+C = multiply(6, 5) 
+D = divide(5, 5)
