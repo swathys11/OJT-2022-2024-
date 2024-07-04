972e6e8987b91e9214bf5c3e283014420c3f0d2e
Swathy S <144905167+swathys11@users.noreply.github.com>
2024-07-04 16:44:27 +0530

    Add files via upload

---
+++ DAY34/OOps-Class.py
@@ -0,0 +1,12 @@
+# Class
+# The class is a blueprint that defines the nature of a future object. A class is a collection of objects 
+
+class MyClass():     #Creating a class
+     "This is a DocString" 
+     x = 5 
+MyClass = MyClass() 
+print(MyClass) 
+print(MyClass.x) 
+print(MyClass.__doc__) 
+
+
